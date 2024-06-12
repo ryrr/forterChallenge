6a13d16f6b5070bff53a65b573942034fd81adad
Ryan Rivera <37811096+ryrr@users.noreply.github.com>
2024-06-12 00:37:01 -0400

    final touches

--- src/api/logic.js
+++ src/api/logic.js
@@ -5,7 +5,7 @@ exports.getIp = async (req,res,next) => {
     let ip = req.params.ipAddress
     let cachedValue = res.locals.cachedValue
     if(cachedValue){
-        res.send({"country_name":cachedValue})
+        res.status(200).send(cachedValue)
     }else{
         let rateLimitExceeded = res.locals.rateLimitExceeded
         if(rateLimitExceeded){
@@ -19,10 +19,13 @@ exports.getIp = async (req,res,next) => {
                 let resp = await axios.get(requestURL)
                 //we need this extra check because for some reason the ipStack API doesn't actually error when the request is bad
                 if(!resp.data.country_name){
-                    res.status(404).send({"error":'Malformed or non-existing IP'})
+                    let errorObj = {"error":'Malformed or non-existing IP'}
+                    cache.ipCache.set(ip,errorObj)
+                    res.status(404).send(errorObj)
                 }else{
-                    cache.ipCache.set(ip,resp.data.country_name)
-                    res.send({"country_name":resp.data.country_name})
+                    let reply = {"country_name":resp.data.country_name}
+                    cache.ipCache.set(ip,reply)
+                    res.status(200).send(reply)
                 }
             }
             catch(e){res.status(500).send('Something broke (probably a malformed URL)!')}
@@ -36,12 +39,10 @@ exports.fallbackGetIp = async (req,res) => {
     //will get caught there and send res. Doing it here would be dead code
     let requestURL = `${config.fallback.baseURL + ip}?` + `${config.fallback.tokenQueryParam}=${config.fallback.apiKey}` 
     try{
-        //*******perhaps delete the inner catch here*********
         let resp = await axios.get(requestURL)
-        if(!resp.data.country){res.status(404).send({"error":'Malformed or non-existing IP'})}
-        else{
-            cache.ipCache.set(ip,resp.data.country)
-            res.send({"country_name":resp.data.country})
-        }
+        let reply = {"country_name":resp.data.country}
+        cache.ipCache.set(ip,reply)
+        res.status(200).send(reply)
+        
     }catch(e){res.status(404).send({"error":'Malformed or non-existing IP'})}
 }
